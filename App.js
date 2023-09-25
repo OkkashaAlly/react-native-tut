80c7847ab25d8b600b536346055a0d103d1c8b47
OkkashaAlly <steamnature006@gmail.com>
2023-09-25 08:10:34 +0300

    10 - Image & ImageBackground components

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { Text, View } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
+import logoImg from "./assets/adaptive-icon.png";
+// const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  console.log("🚀 ~ file: App.js:6 ~ logoImg:", typeof logoImg);
+
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <Text>
-        <Text style={{ color: "white" }}>Hello</Text> World
-      </Text>
+      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} /> */}
+      {/* <Image
+        source={{ uri: "https://picsum.photos/250" }}
+        style={{ width: 250, height: 250 }}
+      /> */}
+      <ImageBackground source={logoImg} style={{flex: 1}}>
+        <Text>Hello bg img</Text>
+      </ImageBackground>
     </View>
   );
 }
