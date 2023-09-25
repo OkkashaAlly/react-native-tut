a36531717a6670e891fac7a61f08ccb5ee9f692a
OkkashaAlly <steamnature006@gmail.com>
2023-09-25 07:42:49 +0300

    9 - Text Components

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import {  View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{backgroundColor: 'plum', flex: 1}}>
-      <View style={{height: 200, width: 200, backgroundColor: "lightblue"}}></View>
-      <View style={{height: 200, width: 200, backgroundColor: "lightgreen"}}></View>
+    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
+      <Text>
+        <Text style={{ color: "white" }}>Hello</Text> World
+      </Text>
     </View>
   );
 }
-
-
