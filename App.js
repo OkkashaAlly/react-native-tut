c6acf3b8542df2194d058d42ed838bd275b21bb8
OkkashaAlly <steamnature006@gmail.com>
2023-09-26 07:19:59 +0300

    14 - Modal component

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import { Image, Pressable, ScrollView, Text, View } from "react-native";
-import logoImg from "./assets/adaptive-icon.png";
+import { useState } from "react";
+import { Button, Modal, Text, View } from "react-native";
 // const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <ScrollView>
-        <Pressable onLongPress={() => alert("image Pressed so long")}>
-          <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        </Pressable>
-        <Pressable onPress={() => alert("pressed text")}>
-          <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-            modi possimus, neque, quas cupiditate sunt iusto cumque repudiandae
-            animi accusamus, quibusdam ad voluptate praesentium temporibus
-            perspiciatis non sequi saepe magni.
-          </Text>
-        </Pressable>
-      </ScrollView>
+      <Button title="Open Modal" onPress={() => setModalVisible(true)} />
+      <Modal
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+        animationType="slide" // "fade" // "none"
+        presentationStyle="pageSheet" // "fullScreen" // "formSheet" // "overFullScreen" // "overCurrentContext" // "currentContext" -- iOS only
+      >
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text>Hello World!</Text>
+          <Button title="Close Modal" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
     </View>
   );
 }
