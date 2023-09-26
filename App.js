9d7e0b12e8a5bfd17a64cd2ad6ab7f44aa040a39
OkkashaAlly <steamnature006@gmail.com>
2023-09-26 07:06:06 +0300

    12 - Button component

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
-import { Image, ScrollView, Text, View } from "react-native";
-import logoImg from "./assets/adaptive-icon.png";
+import { Button, View } from "react-native";
 // const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <ScrollView>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti modi
-          possimus, neque, quas cupiditate sunt iusto cumque repudiandae animi
-          accusamus, quibusdam ad voluptate praesentium temporibus perspiciatis
-          non sequi saepe magni.
-        </Text>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti modi
-          possimus, neque, quas cupiditate sunt iusto cumque repudiandae animi
-          accusamus, quibusdam ad voluptate praesentium temporibus perspiciatis
-          non sequi saepe magni.
-        </Text>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      </ScrollView>
+      <Button
+        title="Click me"
+        onPress={() => alert("Hello World!")}
+        color="green"
+        disabled={false}
+      />
     </View>
   );
 }
