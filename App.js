fcfd8cb51e4d23ecaebbd9bb6c7f11ba074645a8
OkkashaAlly <steamnature006@gmail.com>
2023-09-26 07:11:56 +0300

    13 - Pressable component

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import { Button, View } from "react-native";
+import { Image, Pressable, ScrollView, Text, View } from "react-native";
+import logoImg from "./assets/adaptive-icon.png";
 // const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <Button
-        title="Click me"
-        onPress={() => alert("Hello World!")}
-        color="green"
-        disabled={false}
-      />
+      <ScrollView>
+        <Pressable onLongPress={() => alert("image Pressed so long")}>
+          <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        </Pressable>
+        <Pressable onPress={() => alert("pressed text")}>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            modi possimus, neque, quas cupiditate sunt iusto cumque repudiandae
+            animi accusamus, quibusdam ad voluptate praesentium temporibus
+            perspiciatis non sequi saepe magni.
+          </Text>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 }
