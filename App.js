bfc293f71c5a97e6569d226c920c964459420eee
OkkashaAlly <steamnature006@gmail.com>
2023-09-26 07:45:04 +0300

    20 - StyleSheet API

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
-import { Alert, Button, View } from "react-native";
+import { Alert, Button, Text, View, StyleSheet } from "react-native";
 // const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
 
   return (
-    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <Button title="Alert" onPress={() => Alert.alert("Invalid Data!")} />
-      <Button
-        title="Alert 2"
-        onPress={() => Alert.alert("Invalid Data!", "DOB Incorrect")}
-      />
-      <Button
-        title="Alert 3"
-        onPress={() =>
-          Alert.alert("Invalid Data!", "DOB Incorrect", [
-            { text: "Cancel", onPress: () => console.log("Cancel Pressed") },
-            { text: "OK", onPress: () => console.log("OK Pressed") },
-          ])
-        }
-      />
+    <View style={styles.container}>
+      <Text>Stylesheet API</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "plum",
+    flex: 1,
+    padding: 60,
+  },
+});
