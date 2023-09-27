4ff46f20117178deac23da233197da318373c18d
OkkashaAlly <steamnature006@gmail.com>
2023-09-27 07:12:02 +0300

    37 - Flex Grow

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "lightgreen", flexShrink: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: "lightblue", flexShrink: 1 }}>
+      <Box style={{ backgroundColor: "lightgreen", flexGrow: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "lightblue", flexGrow: 1 }}>
         Box 2
       </Box>
-      <Box style={{ backgroundColor: "pink", flexShrink: 1  }}>Box 3</Box>
-      <Box style={{ backgroundColor: "plum", flexShrink: 1 }}>Box 4</Box>
-      <Box style={{ backgroundColor: "skyblue", flexShrink: 1 }}>Box 5</Box>
-      <Box style={{ backgroundColor: "violet", flexShrink: 1 }}>Box 6</Box>
+      <Box style={{ backgroundColor: "pink", flexGrow: 1  }}>Box 3</Box>
+      <Box style={{ backgroundColor: "plum", flexGrow: 1 }}>Box 4</Box>
+      <Box style={{ backgroundColor: "skyblue", flexGrow: 1 }}>Box 5</Box>
+      <Box style={{ backgroundColor: "violet", flexGrow: 1 }}>Box 6</Box>
     </View>
   );
 }
@@ -24,12 +24,12 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     flex: 1,
     // height: 100,
-    width: 200,
+    // width: 200,
     flexWrap: "nowrap", // "wrap" | "nowrap" | "wrap-reverse"
     alignContent: "flex-start", // "flex-start" | "flex-end" | "center" | "space-between" | "space-around" | "stretch"
-    flexDirection: "row", // "row" | "column" | "row-reverse" | "column-reverse"
+    flexDirection: "column", // "row" | "column" | "row-reverse" | "column-reverse"
     justifyContent: "flex-start", // "flex-start" | "flex-end" | "center" | "space-between" | "space-around" | "space-evenly"
-    alignItems: "flex-start", // "stretch" | "flex-start" | "flex-end" | "center" | "baseline"
+    alignItems: "stretch", // "stretch" | "flex-start" | "flex-end" | "center" | "baseline"
     // rowGap: 10,
     // columnGap: 10,
     // gap: 10,
