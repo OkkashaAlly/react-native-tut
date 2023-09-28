4cc868f59c12f4b50698a21415a0ad0be0ae9fb6
OkkashaAlly <steamnature006@gmail.com>
2023-09-28 07:45:54 +0300

    42 - useWindowDimensions

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
 // const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const { width, height } = useWindowDimensions();
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome DImension</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            height: height > 500 ? "60%" : "90%",
+            width: width > 500 ? "70%" : "90%",
+          },
+        ]}
+      >
+        <Text style={[styles.text, { fontSize: width > 500 ? 50 : 24 }]}>
+          Welcome DImension
+        </Text>
       </View>
     </View>
   );
 }
-
-const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
@@ -23,12 +33,10 @@ const styles = StyleSheet.create({
   box: {
     backgroundColor: "lightblue",
     padding: 20,
-    height: height > 500 ? "60%": '90%',
-    width: width > 500 ? "70%": '90%',
   },
   text: {
     fontWeight: "bold",
-    fontSize: width > 500 ? 50 : 24,
+
     textAlign: "center",
   },
 });
