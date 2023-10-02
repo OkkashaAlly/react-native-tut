8d345d2e51ddd89d92e108d1c2632650b22714b4
OkkashaAlly <steamnature006@gmail.com>
2023-10-02 11:54:19 +0300

    51 - Lists

--- App.jsx
+++ App.jsx
@@ -1,34 +1,40 @@
-import {
-  Platform,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import CustomButton from "./components/CustomButton/CustomButton";
-// const logoImg = require("./assets/adaptive-icon.png");
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
+import pokemon from "./data.json";
 
 export default function App() {
   return (
     // <SafeAreaView> for iOS or android after <StatusBar/>
-    <SafeAreaView style={styles.safeView}>
-      <CustomButton onPress={() => alert("Hello World")} title="Press Me" />
-      <StatusBar style="auto" backgroundColor="plum" />
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+
+      {pokemon.map(pokemon => (
+        <View key={pokemon.id} style={styles.card}>
+          <Text>{pokemon.type}</Text>
+          <Text style={styles.cardText}>{pokemon.name}</Text>
+        </View>
+      ))}
+      </ScrollView>
+      <StatusBar backgroundColor="#f5f5f5" barStyle="dark-content" />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  safeView: {
-    flex: 1,
-    backgroundColor: "plum",
-  },
   container: {
-    backgroundColor: "plum",
     flex: 1,
-    alignItems: "center",
-    paddingTop: Platform.OS === "android" ? 20 : 0,
+    backgroundColor: "#f5f5f5",
   },
-
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+  },
+  cardText: {
+    fontSize: 30,
+  },
 });
