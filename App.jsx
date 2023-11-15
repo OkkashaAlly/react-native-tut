9bad3ae4cca8e0acda8218f2d1fefbf6841a126e
OkkashaAlly <steamnature006@gmail.com>
2023-11-15 07:26:46 +0300

    76 - Stack Navigation Options

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Pressable, Text } from "react-native";
 import AboutScreen from "./screens/AboutScreen";
 import HomeScreen from "./screens/HomeScreen";
 
@@ -8,8 +9,32 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#6a51ae",
+          },
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerRight: () => (
+            <Pressable onPress={() => alert("Menu btn pressed")}>
+              <Text style={{ color: "white", fontSize: 16 }}>Menu</Text>
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: "#e8e4f3",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Welcome home",
+          }}
+        />
         <Stack.Screen
           name="About"
           component={AboutScreen}
