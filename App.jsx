bfa9b545ceba25c296404910b43300771c643ff6
OkkashaAlly <steamnature006@gmail.com>
2023-11-15 06:45:20 +0300

    73 - Stack Navigation

--- App.jsx
+++ App.jsx
@@ -1,7 +1,17 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AboutScreen from "./screens/AboutScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Screen name="about" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
