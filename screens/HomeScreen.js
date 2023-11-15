16aadf7d5f511a1c9593ca0c49f92885299bc920
OkkashaAlly <steamnature006@gmail.com>
2023-11-15 06:42:54 +0300

    72 - Navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,26 @@
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>HomeScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16
+  }
+
+})
+
+export default HomeScreen
