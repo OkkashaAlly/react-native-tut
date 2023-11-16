4d1b6383e15c41ac1319d23d972abf41f55b6942
OkkashaAlly <steamnature006@gmail.com>
2023-11-16 13:57:57 +0300

    81 - Tab Navigation Options

--- App.jsx
+++ App.jsx
@@ -4,14 +4,36 @@ import CourseListScreen from "./screens/CourseListScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelPosition: "below-icon",
+          tabBarShowLabel: true,
+          tabBarActiveTintColor: "purple",
+        }}
+      >
         <Tab.Screen name="Settings" component={SettingsScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "My profile",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person" size={20} color={color} />
+            ),
+            tabBarBadge: 3,
+            tabBarBadgeStyle: {
+              backgroundColor: "green",
+              // color: "red"
+            },
+          }}
+        />
         <Tab.Screen name="CourseList" component={CourseListScreen} />
       </Tab.Navigator>
     </NavigationContainer>
